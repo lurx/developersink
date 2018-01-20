f9cd6f0d45c68424078c9306301c9b22d42423c9
lurx <lurxie@gmail.com>
2018-01-20 22:18:27 +0200

    subnav css

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<input type="checkbox" name="navToggle" id="navToggle" class="navToggle">
+<input type="checkbox" name="navToggle" id="navToggle" class="navToggle" checked>
 <header>
     <a href="<?php echo get_bloginfo( 'wpurl' );?>" class="innerWrapper">
         <label for="navToggle" class="logo"><img src="http://blog.rotemdev.com/wp-content/uploads/2017/06/cropped-plumber-icon-white2.png" alt="" /></label>
@@ -9,7 +9,13 @@
         <ul>
             <li class="active"><span>Blog</span></li>
             <li><span>About</span></li>
-            <li><span>References</span></li>
+            <li>
+                <span>References</span>
+                <ul class="subnav">
+                    <li><span>subitem</span></li>
+                    <li><span>subitem</span></li>
+                </ul>
+            </li>
             <li><span>Contact</span></li>
             <li class="search">
                 <input type="text" placeholder="Search">
